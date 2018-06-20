76a2c549459617c983ad7ed3e96e2def99d2b472
Joseph Hart <josephrhart08@yahoo.com>
2018-06-20 02:03:09 -0500

    adding 'apm install' list

---
+++ apm-install.ps1
@@ -0,0 +1,13 @@
+apm install `
+minimap `
+pigments `
+atom-material-ui `
+atom-material-syntax `
+file-icons `
+seti-ui `
+seti-icons `
+language-babel `
+language-powershell `
+zentabs `
+autocomplete-modules `
+atom-ide-ui
